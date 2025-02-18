6fc5dc1419ddc9748f630d31c2d2f85f68160568
nidhir2401 <nidhir2401@gmail.com>
2025-02-18 22:08:22 +0530

    Second commit

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const path = require('path'); // Add this line
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 // Middleware to parse JSON
 app.use(express.json());
